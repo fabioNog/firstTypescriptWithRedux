d939a3306e3954fab5f602f7e9b0fdf257deac75
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-10-03 09:49:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Alert from './Alert'
 
 const navigation = [
   { name: 'Painel', href: '#dashboard', current: true },
@@ -151,5 +152,6 @@ export default function Example() {
         </>
       )}
     </Disclosure>
+    <Alert/>
   )
 }
